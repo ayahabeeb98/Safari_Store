eb9f3a25dc0cc88767b7b8a5b036ea9f1153a147
mohammed kullab <56482391+mohammedzkullab@users.noreply.github.com>
2021-09-04 17:20:33 +0300

    add header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import './App.css';
-import {BrowserRouter as Router, Switch} from "react-router-dom";
-import {RouteWrapper} from './routes/RouteWrapper';
-import Home from './pages/home';
+import "./App.css";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { RouteWrapper } from "./routes/RouteWrapper";
+import Home from "./pages/home";
+import Navbar from "./components/Navbar";
 import BaseLayout from "./layouts/BaseLayout";
 
 function App() {
   return (
     <Router>
-        <Switch>
-            <RouteWrapper path="/" exact component={Home} layout={BaseLayout}/>
-        </Switch>
+      <Navbar />
+      <Switch>
+        <RouteWrapper path="/" exact component={Home} layout={BaseLayout} />
+      </Switch>
     </Router>
   );
 }
